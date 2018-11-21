3fb8ef85a17d3b7c5125546d7c0cadc40844e190
A.Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2018-11-21 20:43:32 +0300

    Спавн игрока в стартовое положение

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private enum GameState { started, gameOver }
+
+	[SerializeField] private GameObject PlayerPrefab;
+	[SerializeField] private GameObject ChestPrefab;
+	[SerializeField] private Vector2 PlayerSpawnPosition;
+	private GameState _gameState;
+	private GameObject _playerObject;
+
+	private void Start() {
+		_playerObject = Instantiate(PlayerPrefab);
+		_playerObject.transform.localPosition = PlayerSpawnPosition;
+	}
+
+}
